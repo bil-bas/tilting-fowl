2fd589dd98dc53c042e9e508a813c088299223e7
Bil Bas <bil.bagpuss@gmail.com>
2025-08-19 11:19:56 +0100

    removed reference to blocks code

--- main.ts
+++ main.ts
@@ -4,8 +4,10 @@ namespace SpriteKind {
 }
 
 class Player {
-    sprite: Sprite
-    private _next_flap_at: number
+    public sprite: Sprite
+
+    protected next_flap_at: number
+
     constructor(player: number) {
         this.sprite = sprites.create(assets.image`duck left`, player == 1 ? SpriteKind.player1 : SpriteKind.player2)
         if (player == 1) {
@@ -19,30 +21,30 @@ class Player {
             this.add_button_handler(2)
             tiles.placeOnTile(this.sprite, tiles.getTileLocation(9, 6))
         }
-        
+
         this.sprite.setFlag(SpriteFlag.StayInScreen, true)
         this.sprite.ay = 100
         this._next_flap_at = 0
         this.add_overlap_handler()
     }
-    
+
     public add_button_handler(player: number) {
         function on_button_event_a_pressed() {
-            if (game.runtime() > this._next_flap_at) {
+            if (game.runtime() > this.next_flap_at) {
                 this.sprite.vy = -50
-                this._next_flap_at = game.runtime() + 500
+                this.next_flap_at = game.runtime() + 500
             }
-            
+
         }
-        
+
         if (player == 1) {
             controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, on_button_event_a_pressed)
         } else {
             controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, on_button_event_a_pressed)
         }
-        
+
     }
-    
+
     public add_overlap_handler() {
         sprites.onOverlap(SpriteKind.player1, SpriteKind.player2, function on_overlap(p1sprite: Sprite, p2sprite: Sprite) {
             if (p1sprite.y > p2sprite.y) {
@@ -61,7 +63,7 @@ class Player {
                 p2sprite.vx = 50
             }
         })
-    } 
+    }
 }
 
 tiles.loadMap(tiles.createSmallMap(tilemap`level2`))
